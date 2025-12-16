38c7695e0ca9fe7b22c48d63221cbe5df257e797
Ferdinand Dimas <ferdinandimas@gmail.com>
2025-12-17 02:56:34 +0700

    Config variable for persistent DB

--- quiz.py
+++ quiz.py
@@ -11,8 +11,8 @@ APP_TITLE = "Kids Math Quiz"
 DAILY_LIMIT = 400
 REWARD_PER_CORRECT = 50  # rupiah
 COOKIE_NAME = "math_sess"
-DB_PATH = os.getenv("DB_PATH", "math_app.sqlite3")
-ADMIN_CLEAR_PASSWORD = "masukaja"
+DB_PATH = os.getenv("DB_PATH", "/data/math_app.sqlite3")
+ADMIN_CLEAR_PASSWORD = os.getenv("ADMIN_CLEAR_PASSWORD", "masukaja")
 
 CHILDREN = ["althafandra", "alleia"]
 
